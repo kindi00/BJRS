74c60ed79a929a2693b3bcc926f6bcd6ec4264b9
bedikryst <146711978+bedikryst@users.noreply.github.com>
2025-05-16 15:06:18 +0200

    Update 02_data.sql

--- initdb-scripts/02_data.sql
+++ initdb-scripts/02_data.sql
@@ -4,3 +4,5 @@
 
 INSERT INTO ATTENDANCE_TYPES(name) VALUES('Obecny'), ('Nieobecny'), ('Usprawiedliwiony');
 INSERT INTO GENDERS(name) VALUES('kobieta'), ('mężczyzna'), ('dziewczynka'), ('chłopiec');
+
+INSERT INTO FAMILY_MEMBERS(name) VALUES('Dziecko'),('Rodzic'),('Małżonek'),('Brat'),('Siostra'),('Kuzyn'),('Inny') ON CONFLICT (name) DO NOTHING;
